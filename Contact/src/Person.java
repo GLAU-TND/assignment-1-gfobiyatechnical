f55c79e00675d5c2f5e699270e44fce1784322b1
Rakesh Yadav <rakesh.yadav_cs18@gla.ac.in>
2020-02-18 20:20:36 +0530

    Person.java >> getter & setter methods ..init Commit

--- Contact/src/Person.java
+++ Contact/src/Person.java
@@ -4,6 +4,8 @@ public class Person {
     private String firstName;
     private String lastName;
     private String contactNumber;
+    private String anotherContactNumber;
+    private String anotherE_mailAddress;
     private String e_mailAddress;
 
     public static int welcomeContactScreen() {
@@ -17,17 +19,28 @@ public class Person {
                 "Press 5 to exit program \n");
         try {
             userPressed = scan.nextInt();
+            if (userPressed == 1) {
+
+
+            } else if (userPressed == 2) {
+
+            } else if (userPressed == 3) {
+
+            } else if (userPressed == 4) {
+
+            } else if (userPressed == 5) {
+
+            }
         } catch (Exception e) {
             System.out.println("Please Check Your Input :" + e);
         }
         return userPressed;
     }
 
-    public static void addContact() {
-
-    }
-
     public String getFirstName() {
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.next();
+        firstName = "First Name: " + temp;
         return firstName;
     }
 
@@ -35,7 +48,43 @@ public class Person {
         this.firstName = firstName;
     }
 
+    public String getAnotherContactNumber() {
+        String option;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Would you like to add another contact number? (y/n): ");
+            option = scanner.next();
+            if (option.equalsIgnoreCase("y"))
+                anotherContactNumber = getContactNumber();
+            else anotherContactNumber = null;
+        } while (option.equalsIgnoreCase("y"));
+        return anotherContactNumber;
+    }
+
+    public void setAnotherContactNumber(String anotherContactNumber) {
+        this.anotherContactNumber = anotherContactNumber;
+    }
+
+    public String getAnotherE_mailAddress() {
+        String option;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Would you like to add email address? (y/n): ");
+            option = scanner.next();
+            if (option.equalsIgnoreCase("y"))
+                anotherContactNumber = getE_mailAddress();
+            else anotherE_mailAddress = null;
+        } while (option.equalsIgnoreCase("y"));
+        return anotherE_mailAddress;
+    }
+
+    public void setAnotherE_mailAddress(String anotherE_mailAddress) {
+        this.anotherE_mailAddress = anotherE_mailAddress;
+    }
+
     public String getLastName() {
+        Scanner scanner = new Scanner(System.in);
+        lastName = "Last Name: " + scanner.next();
         return lastName;
     }
 
@@ -44,6 +93,8 @@ public class Person {
     }
 
     public String getContactNumber() {
+        Scanner scanner = new Scanner(System.in);
+        contactNumber = "Contact Number: " + scanner.next();
         return contactNumber;
     }
 
@@ -52,10 +103,22 @@ public class Person {
     }
 
     public String getE_mailAddress() {
+        Scanner scanner = new Scanner(System.in);
+        contactNumber = "Email Address: " + scanner.next();
         return e_mailAddress;
     }
 
     public void setE_mailAddress(String e_mailAddress) {
         this.e_mailAddress = e_mailAddress;
+    }
+
+    public void addContact() {
+        System.out.println("You have chosen to add a new contact: \n" +
+                "Please enter the name of the Person");
+        getFirstName();
+        getLastName();
+        getContactNumber();
+        getAnotherContactNumber();
+        getAnotherE_mailAddress();
     }
 }
