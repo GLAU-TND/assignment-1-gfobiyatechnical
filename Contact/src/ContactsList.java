be0793535ff79f887949b7955779bf379cf46c8c
Rakesh Yadav <rakesh.yadav_cs18@gla.ac.in>
2020-02-20 00:45:27 +0530

    ContactsList.java >> searchByFirstName method ...... 01 Commit

--- Contact/src/ContactsList.java
+++ Contact/src/ContactsList.java
@@ -17,6 +17,72 @@ public class ContactsList implements Serializable {
 
     // method to add firstName,lastName,contactNumber,anotherContactNumber,emailAddress and anotherEmailAddress in String form
     public void addInContactsLast(String firstName, String lastName, String contactNumber, String anotherContactNumber, String emailAddress, String anotherEmailAddress) {
+        Person current = head;
+        Person previous = null;
+        Person newNode = new Person();
+
+        // set the data
+        newNode.setFirstName(firstName);
+        newNode.setLastName(lastName);
+        newNode.setContactNumber(contactNumber);
+        newNode.setAnotherContactNumber(anotherContactNumber);
+        newNode.setE_mailAddress(emailAddress);
+        newNode.setAnotherE_mailAddress(anotherEmailAddress);
+
+        if (isEmpty()) {
+            head = newNode;
+            contactLintLength++;
+        } else {
+
+        }
+    }
+
+    // method to use if the linked list is empty
+    public boolean isEmpty() {
+        return (contactLintLength == 0);
+    }
+
+    // method to printContactsList
+    public void printContactsList() {
+        // start at the head and check if there's anything there
+        Person tempNode = head;
+        if (head == null) {
+            System.out.println("The list is empty!");
+        } else {
+
+            for (int i = 0; i < contactLintLength; i++) {
+                System.out.print("Index = " + (i + 1) + " ");
+                System.out.println(tempNode);
+                tempNode = tempNode.getNext();
+            }
+        }
+        System.out.println();
+    }
+
+    // method used to search through the names in the string
+    public void searchByFirstName(String firstName) {
+        Person current = head;
+        boolean empty = true;
+
+        // check to see if the list is empty
+        if (isEmpty()) {
+            System.out.println("The list is empty!");
+        } else {
+            for (int i = 0; i < contactLintLength; i++) {
+                if (current.getFirstName().toLowerCase().contains(firstName.toLowerCase())) {
+                    System.out.print("Index = " + (i + 1) + " ");
+                    System.out.println(current);
+                    empty = false;
+                }
+                // go to the next node
+                current = current.getNext();
+            }
+            // if statement to show if there are no matches found
+            if (empty) {
+                System.out.println("NO RESULTS FOUND!");
+            }
+        }
 
     }
+
 }
