571eb9ecd1be5196677aa6c0ba3051a1bcc986b2
Rakesh Yadav <rakesh.yadav_cs18@gla.ac.in>
2020-02-19 23:21:16 +0530

    Person.java >>     // getter and setter methods          ..commit

--- Contact/src/Person.java
+++ Contact/src/Person.java
@@ -1,4 +1,8 @@
 import java.io.Serializable;
+/*
+author : Rakesh Yadav
+
+ */
 
 class Person implements Serializable {
 
@@ -15,4 +19,60 @@ class Person implements Serializable {
         next = null;
     }
 
+    // getter and setter methods of firstName , lastName , contactNumber , anotherContactNumber , emailAddress and anotherE_mailAddress
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getAnotherContactNumber() {
+        return anotherContactNumber;
+    }
+
+    public void setAnotherContactNumber(String anotherContactNumber) {
+        this.anotherContactNumber = anotherContactNumber;
+    }
+
+    public String getE_mailAddress() {
+        return e_mailAddress;
+    }
+
+    public void setE_mailAddress(String e_mailAddress) {
+        this.e_mailAddress = e_mailAddress;
+    }
+
+    public String getAnotherE_mailAddress() {
+        return anotherE_mailAddress;
+    }
+
+    public void setAnotherE_mailAddress(String anotherE_mailAddress) {
+        this.anotherE_mailAddress = anotherE_mailAddress;
+    }
+
+    public Person getNext() {
+        return next;
+    }
+
+    public void setNext(Person next) {
+        this.next = next;
+    }
 }
