e77701c1617807b2dece1ca97e41ac1bbdc39bc1
Rakesh Yadav <rakesh.yadav_cs18@gla.ac.in>
2020-02-20 15:48:57 +0530

    Main.java >> final Commit

--- Contact/src/Main.java
+++ Contact/src/Main.java
@@ -12,12 +12,38 @@ public class Main implements Serializable {
         Person pObj = new Person();
         int userPressed = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Rakesh's Contact List App\n" +
-                "Press 1 to add a new contact\n" +
-                "Press 2 to view all contacts\n" +
-                "Press 3 to search for a contact\n" +
-                "Press 4 to delete a contact\n" +
-                "Press 5 to exit program ");
-        userPressed = scan.nextInt();
+        do {
+            System.out.println("Welcome to Rakesh's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+            try {
+                userPressed = scan.nextInt();
+            } catch (Exception e) {
+                System.out.println("An Error occurred " + e);
+            }
+            switch (userPressed) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+
+                default:
+                    System.out.println("Unknown Entry ..");
+            }
+        } while (!true);
     }
 }
